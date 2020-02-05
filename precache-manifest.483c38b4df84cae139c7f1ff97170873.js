06205b6bd6f7086cf755d54fd5741feee73ca0cb
metalkick <Bz068@live.mdx.ac.uk>
2020-02-06 04:21:29 +0500

    deploy

---
+++ precache-manifest.483c38b4df84cae139c7f1ff97170873.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfd0bae5ed6116df564",
+    "url": "/css/app.07f624be.css"
+  },
+  {
+    "revision": "4e33de25969ed1e099bfcca0eda5b930",
+    "url": "/img/course/1.jpg"
+  },
+  {
+    "revision": "5cc2fb5ccd1bea54122f4620875ed3ad",
+    "url": "/img/course/10.jpg"
+  },
+  {
+    "revision": "6e6de63b2529e10c79112c12eec5a287",
+    "url": "/img/course/11.jpg"
+  },
+  {
+    "revision": "ee994c952c5dba98a18f6b5c00850242",
+    "url": "/img/course/2.png"
+  },
+  {
+    "revision": "aad6e3ccb5e512907c124ad97b907a12",
+    "url": "/img/course/4.png"
+  },
+  {
+    "revision": "36f1cc6b85ef04741a82a1da8a40662b",
+    "url": "/img/course/5.jpg"
+  },
+  {
+    "revision": "0ad9c6d920c282a3e2439ebb43a30e06",
+    "url": "/img/course/6.jpg"
+  },
+  {
+    "revision": "5ca56ad629b4eaf21b25d713ae49dac2",
+    "url": "/img/course/7.jpg"
+  },
+  {
+    "revision": "139ae68f6b212417f4610071ba7d2d44",
+    "url": "/img/course/9.png"
+  },
+  {
+    "revision": "740a514ef580c2ad0480d5c62dd158fb",
+    "url": "/img/placeholder.png"
+  },
+  {
+    "revision": "a6db99d72065cf94bed9726ff685eaa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dfd0bae5ed6116df564",
+    "url": "/js/app.8080bd90.js"
+  },
+  {
+    "revision": "2a25b6e0eba6e2dcf801",
+    "url": "/js/chunk-vendors.9850081e.js"
+  },
+  {
+    "revision": "5c9f680927e7c843fae6a84ac8d9c239",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
